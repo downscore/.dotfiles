637fc755966b30554b370b8a5fbdeae371ce1e49
Eam <eam.doherty@gmail.com>
2024-07-21 16:14:51 -0700

    Add catppuccin theme and nvim-tree to nvim config.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,8 +58,8 @@ vim.opt.cursorline = true
 -- Minimum number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- Use a default color scheme
-vim.cmd('colorscheme retrobox')
+-- Uncomment to use a default color scheme.
+-- vim.cmd('colorscheme retrobox')
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode.
 vim.opt.hlsearch = true
@@ -115,6 +115,9 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- Allows seamless navigation between tmux and nvim panes.
   { "christoomey/vim-tmux-navigator", },
+
+  -- Catppuccin theme.
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Telescope fuzzy finder.
   {
@@ -175,4 +178,68 @@ require('lazy').setup({
       end, { desc = '[S]earch [N]eovim files' })
     end,
   },
+
+  -- nvim-tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
 })
+
+-- Load the Catppuccin theme.
+require("catppuccin").setup({
+  flavour = "mocha", -- auto, latte, frappe, macchiato, mocha
+  background = { -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = false, -- Disables setting the background color.
+  show_end_of_buffer = false, -- Shows the '~' characters after the end of buffers.
+  term_colors = false, -- Sets terminal colors (e.g. `g:terminal_color_0`).
+  dim_inactive = {
+    enabled = false, -- Dims the background color of inactive window.
+    shade = "dark",
+    percentage = 0.15, -- Percentage of the shade to apply to the inactive window.
+  },
+  no_italic = false, -- Force no italic.
+  no_bold = false, -- Force no bold.
+  no_underline = false, -- Force no underline.
+  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" }, -- Change the style of comments.
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+    -- miscs = {}, -- Uncomment to turn off hard-coded styles.
+  },
+  color_overrides = {},
+  custom_highlights = {},
+  default_integrations = true,
+  integrations = {
+    -- TODO: Enable more integrations when plugins installed.
+    -- cmp = true,
+    -- gitsigns = true,
+    nvimtree = true,
+    -- treesitter = true,
+    -- notify = false,
+    -- mini = {
+    --   enabled = true,
+    --   indentscope_color = "",
+    -- },
+  },
+})
+vim.cmd.colorscheme "catppuccin"
