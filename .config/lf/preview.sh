61e3d131e23fd5391e3574dbaa37891d8fcc7310
Eam <eam.doherty@gmail.com>
2024-08-03 21:32:17 -0700

    Disable line wrapping in lf previews.

--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -2,7 +2,7 @@
 
 # Try to use bat for previews if it is installed.
 if command -v bat > /dev/null; then
-    bat --paging=never --style=numbers --terminal-width $(($2-5)) -f "$1"
+    bat --paging=never --style=numbers --wrap=never --terminal-width $(($2-5)) -f "$1"
 else
     cat "$1"
 fi
