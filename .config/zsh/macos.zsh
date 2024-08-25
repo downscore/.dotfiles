ae4b97edb0454585774230b2fedc6e1930fe9d16
Eam <eam.doherty@gmail.com>
2024-08-24 18:45:33 -0700

    Fix copying commands in fzf in macos.

--- .config/zsh/macos.zsh
+++ .config/zsh/macos.zsh
@@ -18,6 +18,7 @@ export FZF_DEFAULT_OPTS="
   --bind 'ctrl-y:execute-silent(echo {} | pbcopy)+abort'
   --color header:italic
   --header 'Ctrl-Y: Copy'"
+export FZF_CTRL_R_OPTS="--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
 
 # Use bat for man pages if it is available.
 if type bat &>/dev/null; then
