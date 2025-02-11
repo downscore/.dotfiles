1df2d98a3bb3e12d29717c4c0be8a7f650cd47cb
Eam <eam.doherty@gmail.com>
2025-02-10 21:39:58 -0800

    Disable highlighting of current symbol in neovim.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -598,39 +598,36 @@ local main_plugins = {
           -- Go to Declaration. In C this would take you to the header.
           map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-          -- The following two autocommands are used to highlight references of the word under your
-          -- cursor when your cursor rests there for a little while. When you move your cursor, the
-          -- highlights will be cleared (the second autocommand).
+          -- Autocommands for highlighting instances of the word under the cursor.
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if
-            client
-            and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
-          then
-            local highlight_augroup =
-              vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd("LspDetach", {
-              group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds({
-                  group = "kickstart-lsp-highlight",
-                  buffer = event2.buf,
-                })
-              end,
-            })
-          end
+          -- Uncomment to enable highlighting.
+          -- if
+          --   client
+          --   and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
+          -- then
+          --   local highlight_augroup =
+          --     vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+          --   vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          --     buffer = event.buf,
+          --     group = highlight_augroup,
+          --     callback = vim.lsp.buf.document_highlight,
+          --   })
+          --   vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+          --     buffer = event.buf,
+          --     group = highlight_augroup,
+          --     callback = vim.lsp.buf.clear_references,
+          --   })
+          --   vim.api.nvim_create_autocmd("LspDetach", {
+          --     group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+          --     callback = function(event2)
+          --       vim.lsp.buf.clear_references()
+          --       vim.api.nvim_clear_autocmds({
+          --         group = "kickstart-lsp-highlight",
+          --         buffer = event2.buf,
+          --       })
+          --     end,
+          --   })
+          -- end
 
           -- The following code creates a keymap to toggle inlay hints, if the current language
           -- server supports them.
@@ -672,6 +669,15 @@ local main_plugins = {
             Lua = {
               completion = {
                 callSnippet = "Replace",
+              },
+              diagnostics = {
+                globals = { "hs" }, -- Tell LSP to recognize `hs` as a global
+              },
+              workspace = {
+                -- Make the server aware of the Hammerspoon runtime
+                library = {
+                  "/Applications/Hammerspoon.app/Contents/Resources/extensions", -- Path to Hammerspoon's Lua extensions
+                },
               },
               -- Uncomment to ignore Lua_LS's noisy `missing-fields` warnings.
               -- diagnostics = { disable = { 'missing-fields' } },
